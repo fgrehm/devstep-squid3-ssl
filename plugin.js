fd6098eaedff4a4584d13bdfd21d1917e2125aa1
Fabio Rehm <fgrehm@gmail.com>
2015-02-13 00:26:36 -0700

    Disable proxy / caching for localhost requests

--- plugin.js
+++ plugin.js
@@ -6,6 +6,7 @@ devstep.on('configLoaded', function(config) {
     .addLink('squid3:squid3.dev')
     .setEnv('http_proxy', 'http://squid3.dev:3128')
     .setEnv('https_proxy', 'http://squid3.dev:3128')
+    .setEnv('no_proxy', 'localhost,127.0.0.0/8,::1,192.168.0.1,2000::/3,fc00::/8')
     .setEnv('HTTPS_PROXY_CERT', 'squid3.dev.crt')
     .addVolume(squidShared + '/certs/squid3.dev.crt:/usr/share/ca-certificates/squid3.dev.crt')
     .addVolume(squidRoot + '/proxy.sh:/etc/devstep/init.d/proxy.sh');
